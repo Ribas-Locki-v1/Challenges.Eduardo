c9b97538080f786349f359c45e112dcfe4e156ae
Eduardo <ribas@Eduardos-MacBook-Air-2.local>
2026-09-18 10:06:19 +0100

    add missing files

--- 01-what-is-a-cpu/cpu.py
+++ 01-what-is-a-cpu/cpu.py
@@ -2,3 +2,9 @@
 # Print the list, one per line
 # Print a counter for the calculated total list elements
 
+cpulist = ["CPU","ALU", "PC", "MAR", "MDR", "AC", "IR", "DATA BUS", "ADDRESS BUS", "CONTROL BUS"]
+
+for components in cpulist:
+    print(components) 
+
+print("Total number of components, registers and buses in a CPU: ", len(cpulist))
