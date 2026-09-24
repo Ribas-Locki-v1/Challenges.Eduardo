7b753484aeb551f565439cc68123af8ec8b9e45f
Eduardo <ribas@Eduardos-MacBook-Air-2.local>
2026-09-24 15:20:35 +0100

    Added a solution for add digits

--- 03-add-digits/add_digits.py
+++ 03-add-digits/add_digits.py
@@ -1,10 +1,18 @@
 # Given an integer num, repeatedly add all its digits until the result has only one digit, and return it.
 
 def addDigits(num: int) -> int:
-    # add code here
+
     print(num)
-    return 0
+
+    if (num) < 10:
+        return num
+    else:
+        text = str(num)
+        count = 0
+        for i in text:
+            count += int(i)
+        return addDigits(count)
 
 # run tests
 print(addDigits(0))
-print(addDigits(38))
+print(addDigits((99999999999)))
